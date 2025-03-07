aa5bc2507345c28077cd0a9ace960136d107807e
Dan <145234317+finn1817@users.noreply.github.com>
2025-03-07 14:44:57 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7ldjR5FHH109p_43eN75vWysCcoLRnYM",
+  authDomain: "schedule-manager-3f42b.firebaseapp.com",
+  projectId: "schedule-manager-3f42b",
+  storageBucket: "schedule-manager-3f42b.firebasestorage.app",
+  messagingSenderId: "906914836018",
+  appId: "1:906914836018:web:9e68e6d7112da003d4d62b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
